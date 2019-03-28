faf60acfb3f965a1e7d552445343ca40c984af12
Ben Lardie <benlardie@gmail.com>
2019-03-27 21:08:52 -0400

    errors work and go away uppon click

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
 let signIn = document.querySelector('.signin');
 let modal = document.querySelector('body > div');
 let close = document.querySelector('.close');
+let submit = document.querySelector('.submit');
+let inputs = document.querySelectorAll('input');
 
 signIn.addEventListener('click', function() {
     modal.style.display = "inline-block";
@@ -10,6 +12,16 @@ signIn.addEventListener('click', function() {
 
 close.addEventListener('click', function() {
     modal.style.display = 'none';
-})
+});
+
+submit.addEventListener('click', function() {
+    inputs.forEach(function(input) {
+        input.classList.add('error');
+        input.addEventListener('click', function() {
+            input.classList.remove('error');
+        });
+    
+})});
+
 
 });
